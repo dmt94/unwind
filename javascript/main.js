5f65e8a5cdede6abe7c858a58fa00a4b3841a082
Daevah Taylaran <daevtayl@gmail.com>
2022-04-14 17:06:48 -0700

    working

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+document.querySelector('.btn').addEventListener('click', getDrink)
+
+function getDrink() {
+  let drink = document.querySelector('input').value;
+  console.log('clicked');
+
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+  .then(res => res.json()) 
+  .then(data => {
+    console.log(data);
+    // console.log(data.drinks[1]);
+    document.querySelector('h2').innerText = data.drinks[0].strDrink;
+    document.querySelector('.first').src = data.drinks[0].strDrinkThumb;
+    document.querySelector('.instructions').innerText = data.drinks[0].strInstructions;
+  })
+  .catch(err => {
+    console.log(`error ${err}`);
+  });
+}
